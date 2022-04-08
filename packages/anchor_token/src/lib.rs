d152a8146eec05c7b8696291a544534e7c1b6dba
Bharath Vedartham <vedabharath12345@gmail.com>
2022-04-08 10:14:02 +0530

    Remove unecessary files

--- packages/anchor_token/src/lib.rs
+++ packages/anchor_token/src/lib.rs
@@ -1,12 +1,7 @@
-pub mod airdrop;
-pub mod collector;
 pub mod common;
-pub mod community;
-pub mod distributor;
 pub mod gov;
 pub mod querier;
 pub mod staking;
-pub mod vesting;
 
 #[cfg(test)]
 mod mock_querier;
